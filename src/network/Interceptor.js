0802d51ffb58717010b5585cea9b1c4af04b6cdd
Omar Ahmed <omar.ismail277@gmail.com>
2025-07-31 10:30:47 +0300

    Changed Base

--- src/network/Interceptor.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axiosInterceptor = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
-export default axiosInterceptor;
--- src/network/axios.js
+++ src/network/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+const axiosInterceptor = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-export default axiosInstance;
+export default axiosInterceptor;
