e95a99ffa812b20a13099356a181d9d046bb08a3
Omar Ahmed <omar.ismail277@gmail.com>
2025-07-31 10:21:39 +0300

    Test2

--- src/network/Interceptor.js
+++ src/network/Interceptor.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInterceptor = axios.create({
-  baseURL: "https://bookings-json-server-api-production.up.railway.app/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default axiosInterceptor;
